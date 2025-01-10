aee23eb930b90d521bd9b1d45f7d089607b995b7
harshpandey198 <harshpandey198.hp@gmail.com>
2025-01-10 20:49:51 +0530

    git commit -m "Day 8:-ArmstrongNumberChecker"

---
+++ src/Days_1_to_10/ArmstrongNumberChecker.java
@@ -0,0 +1,38 @@
+
+package Days_1_to_10;
+
+import java.util.Scanner;
+
+public class ArmstrongNumberChecker {
+
+        // Method to check if a number is an Armstrong number
+        public static Boolean checkArmstrong ( int number){
+        int temp = number;
+        int sum = 0;
+
+        String str = Integer.toString(number);
+        int size = str.length();
+
+        while (temp > 0) {
+            sum = (int) (sum + Math.pow(temp % 10, size)); // Raise each digit to the power of size and add to sum
+            temp /= 10; // Remove the last digit
+        }
+
+            System.out.println("The sum of the digits raised to the power of the total digits is: " + sum);
+
+        return sum == number; // Return true if the sum matches the original number
+    }
+
+        public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the desired number:");
+        int num = sc.nextInt();
+
+        // Print whether the number is an Armstrong number
+        System.out.println(num + (checkArmstrong(num) ? " is an Armstrong number , as it is matching with the value of number input by user" : " is not an Armstrong number, as it is not matching with the value of number input by user."));
+
+        sc.close(); // Close the scanner
+    }
+    }
+
